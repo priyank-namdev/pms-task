edf268e99cb97484b3244126070ba7b26f2ff6e8
priyank-namdev <priyank.namdev.21@gmail.com>
2025-09-11 02:49:10 +0530

    project and tasks creation

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // output: 'export', // if you are exporting a static build
+  // any other config...
+};
+
+export default nextConfig;
